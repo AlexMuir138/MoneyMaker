dc8fdc5519a009030dd3bda616a5cf18ac7fd1ce
Alex <muirkat138@gmail.com>
2021-10-02 17:01:44 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("How much money would you like to convert to coins?");
+            string userAmount = Console.ReadLine();
+        }
+    }
+}
