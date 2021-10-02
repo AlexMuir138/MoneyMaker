badc1484b0157b5b95437a099beb4a05bf008271
Alex <muirkat138@gmail.com>
2021-10-02 17:07:22 -0600

    first variable

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace MoneyMaker
             Console.WriteLine("Welcome to Money Maker!");
             Console.WriteLine("How much money would you like to convert to coins?");
             string userAmount = Console.ReadLine();
+            double userAmountDouble = Convert.ToDouble(userAmount);
+            Console.WriteLine($"{userAmountDouble} is equal to...");
         }
     }
 }
